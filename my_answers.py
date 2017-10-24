5233596cc51c058d9e53778d4569d747934aee02
kochepasov.viatcheslav <vkochepasov@gmail.com>
2017-10-24 15:41:28 -0400

    completed step 1. Implemented a function to slice data into window time series.

--- my_answers.py
+++ my_answers.py
@@ -9,18 +9,68 @@ import keras
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
-    # containers for input/output pairs
-    X = []
-    y = []
-
-    # reshape each 
-    X = np.asarray(X)
-    X.shape = (np.shape(X)[0:2])
-    y = np.asarray(y)
-    y.shape = (len(y),1)
-
+    ''' separate array like series into set of sliding window data subsets.
+    Horizont sliced implementation.
+    ''' 
+    a = np.asarray(series)
+    n = len(a)
+    step = 1
+    n_strides = int((n-window_size)/step)
+    # np slice [start: stop: step]
+    strides = [series[i: i+n-window_size: step][:, None] for i in range(0, window_size, step)]
+    # stacking window_size number of potentialy long strides 
+    X = np.hstack(strides)
+    # numpy slice [start: stop: step]
+    y = series[window_size: n: step][:, None]
     return X,y
 
+def window_transform_series_index(series, window_size):
+    ''' separate array like series into set of sliding window data subsets.
+    A cross index based implementation.
+    ''' 
+    a = np.asarray(series)
+    n = len(a)
+    step = 1
+    n_strides = int((n-window_size)/step)
+    index = np.arange(window_size)[None, :] + step*np.arange(n_strides)[:, None]
+    X = a[index]
+    # numpy slice [start: stop: step]
+    y = series[window_size: n: step][:, None]
+    return X,y
+
+def window_transform_series_horizont(series, window_size):
+    ''' separate array like series into set of sliding window data subsets.
+    Horizont sliced implementation.
+    ''' 
+    a = np.asarray(series)
+    n = len(a)
+    step = 1
+    n_strides = int((n-window_size)/step)
+    # np slice [start: stop: step]
+    strides = [series[i: i+n-window_size: step][:, None] for i in range(0, window_size, step)]
+    # stacking window_size number of potentialy long strides 
+    X = np.hstack(strides)
+    # numpy slice [start: stop: step]
+    y = series[window_size: n: step][:, None]
+    return X,y
+
+def window_transform_series_vertical(series, window_size):
+    ''' separate array like series into set of sliding window data subsets.
+    Vertical sliced implementation.
+    ''' 
+    a = np.asarray(series)
+    n = len(a)
+    step = 1
+    n_strides = int((n-window_size)/step)
+    # each slice is a window rooling over n_strides [start: stop: step]
+    strides = [series[i: i+window_size: step] for i in range(0, n_strides)]
+    # stacking potentialy big number of window_size strides 
+    X = np.vstack(strides)
+    # numpy slice [start: stop: step]
+    y = series[window_size: n: step][:, None]
+    return X,y
+
+    
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
     pass
